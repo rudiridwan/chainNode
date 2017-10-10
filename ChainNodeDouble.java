1ec16d51367d8ec2d58eb6e9b46ce8420dba9071
Rudi Ridwan <ridwan28ixg@gmail.com>
2017-10-10 20:39:24 +0700

    Add files via upload

---
+++ ChainNodeDouble.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3;
+
+/**
+ *
+ * @author Fiki L
+ */
+public class ChainNodeDouble {
+    Object element;
+    ChainNodeDouble next;
+    ChainNodeDouble prev;
+    
+    ChainNodeDouble(){
+        
+    }
+    ChainNodeDouble(Object Element){
+        this.element = element;
+    }
+
+    public ChainNodeDouble(Object element, ChainNodeDouble next) {
+        this.element = element;
+        this.next = next;
+    }
+    public ChainNodeDouble(Object element, ChainNodeDouble next, ChainNodeDouble prev) {
+        this.element = element;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public ChainNodeDouble getNext() {
+        return next;
+    }
+
+    public void setNext(ChainNodeDouble next) {
+        this.next = next;
+    }
+
+    public ChainNodeDouble getPrev() {
+        return prev;
+    }
+
+    public void setPrev(ChainNodeDouble prev) {
+        this.prev = prev;
+    }
+    
+    
+}
